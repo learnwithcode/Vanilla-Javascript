e8a3c4bff270d84bcd2aebb47210abb1a1be2b94
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-11-15 18:56:17 +0530

    Delete Filter Tasks

--- ToDoApp/task.js
+++ ToDoApp/task.js
@@ -12,7 +12,13 @@ loadeventListners();
 function loadeventListners() {
   // Add task event
   form.addEventListener('submit', addTask);
-}
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear  task event
+  clearBtn.addEventListener('click', clearTask);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
+};
 
 // Add task
 function addTask(e) {
@@ -37,4 +43,38 @@ function addTask(e) {
   taskList.appendChild(li);
   taskInput.value = '';
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item'))
+    if (confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  console.log(e.target);
+}
+
+// Clear Task
+function clearTask() {
+  // one way
+  //taskList.innerHTML = '';
+
+  // better way
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
